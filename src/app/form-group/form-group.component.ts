2260ea180ba1b59dd468f0f4dda70d1e8a0f8265
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-05-10 10:12:51 +0200

    formulaire + reset
    Un Formulaire qui retourne les information dans la console et qui les reset dans le input html

--- src/app/form-group/form-group.component.ts
+++ src/app/form-group/form-group.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-form-group',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormGroupComponent implements OnInit {
 
+  ourform = new FormGroup({
+    firstname: new FormControl(''),
+    lastname: new FormControl(''),
+    email: new FormControl('')
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addClient(): void {
+    console.log(this.ourform.value);
+    /*const formValues = this.ourform.value;
+    const c = new Client(formValues.firstName, formValues.lastName, formValues.email);
+    /this.clientService.addClient(c);*/
+    this.ourform.reset();
   }
 
 }
